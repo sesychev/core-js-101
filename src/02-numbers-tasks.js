3b15508d259b54607993b739f60972969808f08f
sesychev <sergeysychou@gmail.com>
2022-07-26 00:42:32 +0300

    feat: update core-js-101

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -218,8 +218,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number.parseInt(value, 10) || def;
 }
 
 module.exports = {
