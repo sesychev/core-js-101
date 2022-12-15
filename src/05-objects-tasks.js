656c1fa0f03cfc75508826d568e517d516df5bad
sesychev <sergeysychou@gmail.com>
2022-12-15 21:42:37 +0300

    feat: getJSON done

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -20,7 +20,6 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-  // throw new Error('Not implemented');
   this.width = width;
   this.height = height;
   this.getArea = () => this.width * this.height;
@@ -38,8 +37,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
