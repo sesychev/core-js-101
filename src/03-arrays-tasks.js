90e613aa4b8f96450e3bf525179a167cc3e05470
sesychev <sergeysychou@gmail.com>
2022-12-20 16:36:31 +0300

    feat: generateOdds done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -34,8 +34,8 @@ function findElement(arr, value) {
  *    2 => [ 1, 3 ]
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array(len).fill(0).map((_, item) => 1 + 2 * item); // 0, 1, 2, 3, 4
 }
 
 /**
