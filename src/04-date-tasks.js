a458590d096b00839d0da4f17a47a2c64438f4c0
sesychev <sergeysychou@gmail.com>
2022-12-20 14:02:43 +0300

    feat: timeSpanToString done

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -76,8 +76,11 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  function addLeadingZeros(num, totalLength) {
+    return String(num).padStart(totalLength, '0');
+  }
+  return `${addLeadingZeros((endDate.getHours() - startDate.getHours()), 2)}:${addLeadingZeros((endDate.getMinutes() - startDate.getMinutes()), 2)}:${addLeadingZeros((endDate.getSeconds() - startDate.getSeconds()), 2)}.${addLeadingZeros((endDate.getMilliseconds() - startDate.getMilliseconds()), 3)}`;
 }
 
 
