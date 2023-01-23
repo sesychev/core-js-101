78aec773af5a09efb6e326a5e670c13fa5bb45d6
sesychev <sergeysychou@gmail.com>
2023-01-23 11:49:55 +0300

    feat: getIdGeneratorFunction done

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -175,8 +175,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let generator = startFrom - 1;
+  return () => {
+    generator += 1;
+    return generator;
+  };
 }
 
 
